5bd18a0ce67d76268a64505159a06a36b9e0d77f
glisanti@gmail.com <glisanti@gmail.com>
2017-04-05 13:31:27 +0200

    fixed map issue

--- script.js
+++ script.js
@@ -24,3 +24,10 @@ $(document).ready(function(){
     }
 
 });
+
+
+$('.map-container')
+	.click(function(){
+			$(this).find('iframe').addClass('clicked')})
+	.mouseleave(function(){
+			$(this).find('iframe').removeClass('clicked')});
